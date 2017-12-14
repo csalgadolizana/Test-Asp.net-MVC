707648a97774df1a7850f006d37fdc14c5437433
csalgadolizana <csalgadolizana@gmail.com>
2017-12-14 18:12:38 -0300

    primer commit

---
+++ WebApplication1/Controllers/AjaxController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AjaxController : Controller
+    {
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Vista1()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(PersonaModel p)
+        {
+            string nombre = p.Name;
+            string fecha = DateTime.Now.ToString();
+            p.Fecha = fecha;
+
+            return View(p);
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod(string name)
+        {
+            Console.WriteLine("Lllegooo al metodo");
+            PersonaModel p = new PersonaModel
+            {
+                Name = name,
+                Fecha = DateTime.Now.ToString()
+            };
+            return Json(p);
+        }
+    }
+}
---
+++ WebApplication1/Models/PersonaModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class PersonaModel
+    {
+        public string Name{ get; set; }
+        public string Fecha { get; set; }
+
+    }
+}
